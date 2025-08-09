83669bd20c7a27778e9013b3012496cdd1584314
nuhan021 <baibur1216@gmai.com>
2025-08-08 21:52:15 -0700

    v-0.01

---
+++ lib/core/services/router/router.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_tdd_arc/core/services/dependencies/injection_container.dart';
+import 'package:bloc_tdd_arc/core/services/router/app_routes.dart';
+import 'package:bloc_tdd_arc/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:bloc_tdd_arc/features/auth/presentation/views/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'router.main.dart';
